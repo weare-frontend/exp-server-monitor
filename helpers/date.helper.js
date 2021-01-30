666218722033115a687e3e6e1f9041a33c029c00
nottdark <nottdark.dev@gmail.com>
2021-01-31 05:11:52 +0700

    Fix date helper

--- helpers/date.helper.js
+++ helpers/date.helper.js
@@ -1,41 +1,40 @@
 const moment = require('moment')
-const YEAR_AD = 543;
+const YEAR_AD = 543
 
 const methods = {
-
-  toDate({_d, locale = 'en', isShortness = false} = {}) {
+  toDate({ _d, locale = 'en', isShortness = false } = {}) {
     if (!_d) return ''
     let format = isShortness ? 'DD MMM YYYY' : 'DD MMMM YYYY'
     try {
       switch (locale.toLowerCase()) {
         case 'th':
-          moment.locale('th');
-          return moment(_d).add(YEAR_AD, 'years').format(format);
+          moment.locale('th')
+          return moment(_d).add(YEAR_AD, 'years').utcOffset('+0700').format(format)
         default:
-          moment.locale('en');
-          return moment(_d).format(format)
+          moment.locale('en')
+          return moment(_d).utcOffset('+0700').format(format)
       }
     } catch (error) {
       return ''
     }
   },
-  
-  toDateTime({_d, locale = 'en', isShortness = false} = {}) {
+
+  toDateTime({ _d, locale = 'en', isShortness = false } = {}) {
     if (!_d) return ''
     let format = isShortness ? 'DD MMM YYYY HH:mm' : 'DD MMMM YYYY HH:mm'
     try {
       switch (locale.toLowerCase()) {
         case 'th':
-          moment.locale('th');
-          return moment(_d).add(YEAR_AD, 'years').format(format);
+          moment.locale('th')
+          return moment(_d).add(YEAR_AD, 'years').utcOffset('+0700').format(format)
         default:
-          moment.locale('en');
-          return moment(_d).format(format)
+          moment.locale('en')
+          return moment(_d).utcOffset('+0700').format(format)
       }
     } catch (error) {
       return ''
     }
-  }
+  },
 }
 
 module.exports = { ...methods }
